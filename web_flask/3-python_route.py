45d99e076917a79c1d6b16435a4ce904839d1955
Babuhatarii <ochegoharyon@gmail.com>
2024-03-26 05:11:50 -0700

    number route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route that displays "Hello HBNB!"
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+# Define a route that displays "HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+""" Define a route that takes a text variable :\
+        and displays "C " followed by the value """
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    text = text.replace('_', ' ')  # Replace underscores with spaces
+    return "C " + text
+
+
+"""  Define a route that takes a text variable and displays:\
+        "Python " followed by the value"""
+# If no text is provided, use the default value "is cool"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    text = text.replace('_', ' ')  # Replace underscores with spaces
+    return "Python " + text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route that displays "Hello HBNB!"
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+# Define a route that displays "HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+# Define a route that takes a text variable
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    text = text.replace('_', ' ')
+    return "C " + text
+
+# Define a route that takes a text variable
+# If no text is provided, use the default value "is cool"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    text = text.replace('_', ' ')
+    return "Python " + text
+
+# Define a route that takes an integer
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
