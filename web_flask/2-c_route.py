6d3dcfcd50772bb79f53420c0e643a55fa1f995b
Babuhatarii <ochegoharyon@gmail.com>
2024-03-26 04:56:08 -0700

    c is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" 
+This Script starts a flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route that displays "Hello HBNB!"
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+# Define a route that displays "HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    text = text.replace('_', ' ')  # Replace underscores with spaces
+    return "C " + text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
