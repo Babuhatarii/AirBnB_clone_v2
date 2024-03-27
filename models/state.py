cdd1430058acd1844c3671eba18bb400659e2b59
Babuhatarii <ochegoharyon@gmail.com>
2024-03-27 00:23:12 -0700

    state.py

--- models/state.py
+++ models/state.py
@@ -1,32 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-import os
-from sqlalchemy.orm import relationship
+"""Defines the State class."""
+import models
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.city import City
-
-storeType = os.environ.get('HBNB_TYPE_STORAGE')
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    name = Column('name', String(128), nullable=False)
-    __tablename__ = 'states'
-    cities = relationship(
-        'City',
-        backref='state',
-        cascade='all, delete, delete-orphan',
-        passive_deletes=True
-    )
+    """Represents a state for a MySQL database.
 
-    @property
-    def cities(self):
-        """cities filestorage getter function"""
-        import models
-        cityObjects = []
-        results = models.storage.all('City').values()
-        for c in results:
-            if c.state_id == self.id:
-                cityObjects.append(c)
-        return cityObjects
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+    Attributes:
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
